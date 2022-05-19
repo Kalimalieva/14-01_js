80244307b0fa3211b56ceb829adbf26a83e7a78d
Aigul Kalimalieva <aigulkalimalieva@Aiguls-MacBook-Air.local>
2022-05-20 00:15:35 +0600

    hw4

--- HW-4/script.js
+++ HW-4/script.js
@@ -1,35 +1,18 @@
+let counter = document.querySelector('.counter');
+let count = 0;
 
-let div = document.createElement("div")
-div.className = "counter"
-let blocks  = document.createElement("div")
-blocks.className = "blocks"
-let button = document.createElement("button")
-button.innerHTML = "+"
-let num = document.createElement("span")
-num.innerHTML = "0"
-let button1 = document.createElement("button")
-button1.innerHTML = "-"
+function changeCounter (button) {
+    if (button === plus) {
+        count ++;
+    }else if (button === minus) {
+        if (count > 0) {
+            count -= 1;
+        }
+    }else if (button === reset) {
+        count = 0;
+    }
 
-
-document.body.append(div)
-div.append(blocks)
-blocks.append(button)
-blocks.append(num)
-blocks.append(button1)
-
-
-let counter = 0;
-
-function oneCount(){
-    counter++;
-    num.innerText = counter;
+    counter.innerHTML = count;
 }
 
-function twoCount() {
-   counter--;
-   num.innerText = counter;
-}
-
-button.addEventListener("click", oneCount);
-button1.addEventListener("click", twoCount);
 
